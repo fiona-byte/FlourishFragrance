e7bf384e821a305a6bde403777b4311661e07477
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-07-26 14:55:12 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import MainApp from './container/mainApp'
+import Footer from './components/Footer/footer'
+import Header from './pages/Header/header'
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MainApp /> */}
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
