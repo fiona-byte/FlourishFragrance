bf636c20d96717a8428e9d3954361ead7c4aadd1
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-08-30 20:39:04 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-// import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer/footer';
 import Container from './container';
 import Header from './pages/Header/header';
 
 function App() {
-  // const queryClient = new QueryClient();
   return (
     <BrowserRouter>
       <Container />
-      {/* <QueryClientProvider client={queryClient}
-        <Container />
-      </QueryClientProvider> */}
     </BrowserRouter>
   );
 }
