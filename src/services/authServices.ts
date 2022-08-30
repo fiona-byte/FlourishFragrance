ad7aba2a4ff25988e58c00781c712a3bc565bbbc
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-08-30 21:38:59 +0100

    auth with google

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,6 +1,12 @@
-import { auth, db } from '../config';
+import { auth, db, provider } from '../config';
 import { doc, setDoc } from 'firebase/firestore';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from 'firebase/auth';
 
 type registerPayload = {
   firstName: string;
@@ -16,15 +22,31 @@ type signInPayload = {
 
 export const authServices = {
   async register({ firstName, lastName, email, password }: registerPayload) {
-    return await createUserWithEmailAndPassword(auth, email, password).then((response) => {
-      let userId = response.user.uid;
-      setDoc(doc(db, 'users', userId), { firstName, lastName });
-      return response;
-    });
+    return await createUserWithEmailAndPassword(auth, email, password)
+      .then((response) => {
+        let userId = response.user.uid;
+        setDoc(doc(db, 'users', userId), { firstName, lastName });
+        return response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
   async login({ email, password }: signInPayload) {
-    const user = await signInWithEmailAndPassword(auth, email, password);
-    return user;
+    return await signInWithEmailAndPassword(auth, email, password);
+  },
+  async authWithGoogle() {
+    return await signInWithPopup(auth, provider)
+      .then((response) => {
+        const credential = GoogleAuthProvider.credentialFromResult(response);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = response.user;
+        return user;
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   },
   async logout() {
     signOut(auth);
