2fcdabca88039f6719767822f99523b9984a40d2
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-12-21 18:55:21 +0100

    Design bug fixes

--- src/pages/ProductsPage/products-page.tsx
+++ src/pages/ProductsPage/products-page.tsx
@@ -1,25 +1,36 @@
 import ProductCard from '../../components/ProductCard/card';
 import productImg from '../../assets/imgs/product img.png';
 import {
+  ProductsBox,
   ProductsContainer,
+  ProductsFilterContainer,
+  ProductsFilterList,
   ProductsSearchContainer,
   ProductsSearchInput,
   ProductsWrapper,
 } from './products-page.styles';
 import SearchIcon from '../../assets/svgs/search';
+import Pagination from '../../components/Pagination/pagination';
 
 const ProductsPage = () => {
   return (
     <ProductsContainer>
       <h2 className='heading'>Our Products</h2>
-      <ProductsSearchContainer>
-        <SearchIcon className='search__icon' />
-        <ProductsSearchInput type='search' placeholder='Search products' />
-      </ProductsSearchContainer>
+      <ProductsBox>
+        <ProductsSearchContainer>
+          <SearchIcon className='search__icon' />
+          <ProductsSearchInput type='search' placeholder='Search products' />
+        </ProductsSearchContainer>
+        <ProductsFilterContainer>
+          <ProductsFilterList className='active'>All</ProductsFilterList>
+          <ProductsFilterList>Most Popular</ProductsFilterList>
+          <ProductsFilterList>Best Sellers</ProductsFilterList>
+        </ProductsFilterContainer>
+      </ProductsBox>
       <ProductsWrapper>
         <ProductCard productName='Victoria Secret Bombshell' productPrice='₦10,000' productImage={productImg} />
-        <ProductCard productName='Victoria Secret Bombshell' productPrice='₦20,000' productImage={productImg} />
       </ProductsWrapper>
+      <Pagination />
     </ProductsContainer>
   );
 };
