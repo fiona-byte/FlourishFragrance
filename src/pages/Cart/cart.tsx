f05d98347e04f4410f1b9fe7d4e0daca8cc69341
devfiona <54639346+fiona-byte@users.noreply.github.com>
2022-12-28 00:08:55 +0100

    Updates

--- src/pages/Cart/cart.tsx
+++ src/pages/Cart/cart.tsx
@@ -23,58 +23,68 @@ import { useMobile } from '../../hooks/useMobile';
 import productImg from '../../assets/imgs/product img.png';
 import IncrementSwitch from '../../components/IncrementSwitch/increment-switch';
 import Trash from '../../assets/svgs/trash';
+import EmptyCart from '../EmptyCart/empty-cart';
 
 const Cart = () => {
   const isMobile = useMobile();
+
+  const product = 0;
+
   return (
-    <CartWrapper>
-      <CartHeading>Cart</CartHeading>
-      <div>
-        <CartBox>
-          <CartControl>
-            <CartCard>
-              <div style={{ position: 'relative' }}>
-                <CartImage src={productImg} alt='product' />
-                <ProductCount>1</ProductCount>
-              </div>
-              <CartProductInfo>
-                <div>
-                  <CartProductName>Victora Secret Bombshell</CartProductName>
-                  <CartPrice>₦10,000</CartPrice>
+    <>
+      {product === 0 ? (
+        <EmptyCart />
+      ) : (
+        <CartWrapper>
+          <CartHeading>Cart</CartHeading>
+          <div>
+            <CartBox>
+              <CartControl>
+                <CartCard>
+                  <div style={{ position: 'relative' }}>
+                    <CartImage src={productImg} alt='product' />
+                    <ProductCount>1</ProductCount>
+                  </div>
+                  <CartProductInfo>
+                    <div>
+                      <CartProductName>Victora Secret Bombshell</CartProductName>
+                      <CartPrice>₦10,000</CartPrice>
+                    </div>
+                    <IncrementSwitch />
+                  </CartProductInfo>
+                </CartCard>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    paddingTop: '2.4rem',
+                  }}
+                >
+                  {!isMobile && <ProductPrice>₦10,000</ProductPrice>}
+                  <CartRemoveButton>
+                    <Trash />
+                    <span>Remove</span>
+                  </CartRemoveButton>
                 </div>
-                <IncrementSwitch />
-              </CartProductInfo>
-            </CartCard>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                paddingTop: '2.4rem',
-              }}
-            >
-              {!isMobile && <ProductPrice>₦10,000</ProductPrice>}
-              <CartRemoveButton>
-                <Trash />
-                <span>Remove</span>
-              </CartRemoveButton>
-            </div>
-          </CartControl>
-        </CartBox>
-      </div>
-      <CartContainer>
-        <CartContainerText>Subtotal:</CartContainerText>
-        <CartContainerTotal>₦10,000</CartContainerTotal>
-      </CartContainer>
-      <CartButtonContainer>
-        {isMobile ? (
-          <CartLink to='/products'>Continue Shopping</CartLink>
-        ) : (
-          <CartOutlinedButton to='/products'>Continue shopping</CartOutlinedButton>
-        )}
-        <CartFilledButton>Checkout</CartFilledButton>
-      </CartButtonContainer>
-    </CartWrapper>
+              </CartControl>
+            </CartBox>
+          </div>
+          <CartContainer>
+            <CartContainerText>Subtotal:</CartContainerText>
+            <CartContainerTotal>₦10,000</CartContainerTotal>
+          </CartContainer>
+          <CartButtonContainer>
+            {isMobile ? (
+              <CartLink to='/products'>Continue Shopping</CartLink>
+            ) : (
+              <CartOutlinedButton to='/products'>Continue shopping</CartOutlinedButton>
+            )}
+            <CartFilledButton>Checkout</CartFilledButton>
+          </CartButtonContainer>
+        </CartWrapper>
+      )}
+    </>
   );
 };
 
